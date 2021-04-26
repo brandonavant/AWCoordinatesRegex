f312d6ac4d9e7c3db47dd743484c6f7e610d95b0
Brandon Avant <avant.brandon@gmail.com>
2021-04-25 19:31:47 -0500

    Cleaned up tests. Need to account for negatives.

--- test.js
+++ test.js
@@ -15,55 +15,96 @@ const groups = {
 };
 
 /**
- * Tests that given only N and W values ), the X and Z are matched accordingly.
+ * Tests that given only N and W values (no decimals), the X and Z are matched accordingly.
  */
-it('North and West, No Decimals; X and Z are parsed.', () => {
+it('coordinateRegex_northAndWestNoDecimals_parsesSuccessfully', () => {
     let coordinates = parseCoordinates('120N 130W');
 
-    expect(coordinates.x).toBe('130W');
-    expect(coordinates.y).toBe(undefined);
-    expect(coordinates.z).toBe('120N');
-    expect(coordinates.yaw).toBe(undefined);
+    expect(coordinates.x).toBe(130);
+    expect(coordinates.y).toBe(0);
+    expect(coordinates.z).toBe(120);
+    expect(coordinates.yaw).toBe(0);
 });
 
 /**
- * Tests that given only S and E values, the X and Z are matched accordingly.
+ * Tests that given only S and E values (no decimals), the X and Z are matched accordingly.
  */
-it('South and East, No Decimals; X and Z are parsed.', () => {
+it('coordinateRegex_southAndEastNoDecimals_parsesSuccessfully', () => {
     let coordinates = parseCoordinates('120S 130E');
 
-    expect(coordinates.x).toBe('130E');
-    expect(coordinates.y).toBe(undefined);
-    expect(coordinates.z).toBe('120S');
-    expect(coordinates.yaw).toBe(undefined);
+    expect(coordinates.x).toBe(130);
+    expect(coordinates.y).toBe(0);
+    expect(coordinates.z).toBe(120);
+    expect(coordinates.yaw).toBe(0);
 });
 
 /**
- * Tests that given 
+ * Tests that given only N and W values (with decimals), the X and Z are matched accordingly.
  */
-it('North and West, Decimals; X and Z are parsed.', () => {
+it('coordinateRegex_northAndWestWithDecimals_parsesSuccessfully', () => {
     let coordinates = parseCoordinates('120.58N 4.62W');
 
-    expect(coordinates.x).toBe('4.62W');
-    expect(coordinates.y).toBe(undefined);
-    expect(coordinates.z).toBe('120.58N');
-    expect(coordinates.yaw).toBe(undefined);
+    expect(coordinates.x).toBe(4.62);
+    expect(coordinates.y).toBe(0);
+    expect(coordinates.z).toBe(120.58);
+    expect(coordinates.yaw).toBe(0);
+});
+
+/**
+ * Tests that given all values (with no decimals), the X, Y, Z, and Yaw are matched accordingly.
+ */
+it('coordinateRegex_allValuesNoDecimals_parsesSuccessfully', () => {
+    let coordinates = parseCoordinates('120N 4W 0A 3599');
+
+    expect(coordinates.x).toBe(4);
+    expect(coordinates.y).toBe(0);
+    expect(coordinates.z).toBe(120);
+    expect(coordinates.yaw).toBe(3599);
+});
+
+it('coordinateRegex_allValuesWithDecimals_parsesSuccessfully', () => {
+    let coordinates = parseCoordinates('145.76S 175.34E 120.44A 2700');
+
+    expect(coordinates.x).toBe(175.34);
+    expect(coordinates.y).toBe(120.44);
+    expect(coordinates.z).toBe(145.76);
+    expect(coordinates.yaw).toBe(2700);
+});
+
+/**
+ * Calling getValueFromCoordinatePiece with a properly formatted portion of coordinates
+ * parses the value out successfully.
+ */
+it('getValueFromCoordinatePiece_validCoordinatePiece_parsesSuccessfully', () => {
+    let value = getValueFromCoordinatePiece('120W');
+
+    expect(value).toBe(120);
 });
 
 /**
  * Takes a given coordinate string value and uses the coordinate regex
  * to parse out the pieces.
- * @param {String} value 
- * @returns An object containing the X, Y, Z, and Yaw values
+ * @param {String} value The value from which the coordinates are parsed.
+ * @returns An object containing the X, Y, Z, and Yaw valuesW
  * parsed from the given coordinate string.
  */
 function parseCoordinates(value) {
     let execResults = coordinateRegex.exec(value);
 
     return {
-        x: execResults[groups.EW],
-        y: execResults[groups.ALT],
-        z: execResults[groups.NS],
-        yaw: execResults[groups.YAW]
+        x: getValueFromCoordinatePiece(execResults[groups.EW]),
+        y: getValueFromCoordinatePiece(execResults[groups.ALT]),
+        z: getValueFromCoordinatePiece(execResults[groups.NS]),
+        yaw: getValueFromCoordinatePiece(execResults[groups.YAW])
     };
+}
+
+/**
+ * Parses the numeric portion of a coordinate string.
+ * @param {String} value The value from which numbers are parsed.
+ * @returns The coordinate value from the formatted string.
+ */
+function getValueFromCoordinatePiece(value) {
+    let matches = /^\s*\d*\.?\d+/.exec(value);
+    return matches ? parseFloat(matches[0]) : 0;
 }
